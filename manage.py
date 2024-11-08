a1844938ee774d4beea71df12f9f89b61b49fe55
Cik232 <azamjon23202@gmail.com>
2024-11-08 15:55:00 +0500

    change project

--- manage.py
+++ manage.py
@@ -17,9 +17,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-from django.core.management import execute_from_command_line
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
-    from promo.celery import app  # Celery ilovasini import qilish
 
-    execute_from_command_line(sys.argv)
+if __name__ == '__main__':
+    main()
