789eb36606552ad1aee94307dcdddc2ed11cf519
Ziyodillo Yigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-11-12 23:41:48 +0500

    modified urls.py

--- promo/urls.py
+++ promo/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('promo-entries/monthly/', PromoMonthlyView.as_view()),
     path('promo/', PromoEntryList.as_view()),
     path('Promo-add/', PromoCreateView.as_view()),
-    path("fetch-data/", FetchAndSaveDataView.as_view(), name="fetch_data"),
     path('reset-notification/', ResetNotificationView.as_view(), name='daily-request'),
 
 ]
